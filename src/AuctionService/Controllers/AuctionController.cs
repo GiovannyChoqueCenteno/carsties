9db71388fd184c3ae509063a670bf8e824761339
Giovanny Choque Centeno <giovannyjchoquecenteno@gmail.com>
2025-02-24 18:35:00 -0400

    Añadiendo SearchService

--- src/AuctionService/Controllers/AuctionController.cs
+++ src/AuctionService/Controllers/AuctionController.cs
@@ -1,5 +1,6 @@
 using AuctionService.Data;
 using AuctionService.DTOs;
+using AuctionService.Entities;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -7,30 +8,72 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AuctionService.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/auctions")]
     [ApiController]
     public class AuctionController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
         private readonly IMapper _mapper;
 
-        public AuctionController(ApplicationDbContext context,IMapper mapper)
+        public AuctionController(ApplicationDbContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
         }
         [HttpGet]
-        public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions(){
-            var auctions = await _context.Auctions.Include( x => x.Item)
+        public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+        {
+            var auctions = await _context.Auctions.Include(x => x.Item)
             .OrderBy(x => x.Item.Make)
             .ToListAsync();
             return _mapper.Map<List<AuctionDto>>(auctions);
         }
         [HttpGet("{id}")]
-        public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id){
-            var auctions = await _context.Auctions.Include( x => x.Item)
-            .FirstOrDefaultAsync(x => x.Id ==id);
+        public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+        {
+            var auctions = await _context.Auctions.Include(x => x.Item)
+            .FirstOrDefaultAsync(x => x.Id == id);
             return _mapper.Map<AuctionDto>(auctions);
+        }
+        [HttpPost]
+        public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto createAuctionDto)
+        {
+            var auction = _mapper.Map<Auction>(createAuctionDto);
+            auction.Seller = "test";
+            _context.Auctions.Add(auction);
+            var result = await _context.SaveChangesAsync() > 0;
+            if (!result) return BadRequest("Failed to create auction");
+            return CreatedAtAction(nameof(GetAuctionById), new { id = auction.Id }, _mapper.Map<AuctionDto>(auction));
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<AuctionDto>> UpdateAuction(Guid id, UpdateActionDto updateAuctionDto)
+        {
+            var auction = await _context.Auctions.Include(x => x.Item)
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (auction == null) return NotFound();
+
+            auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+            auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+            auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+            auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+            auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+
+            var result = await _context.SaveChangesAsync() > 0;
+            if (!result) return BadRequest("Failed to update auction");
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<AuctionDto>> DeleteAction(Guid id, UpdateActionDto updateAuctionDto)
+        {
+            var auction = await _context.Auctions.FindAsync(id);
+            if (auction == null) return NotFound();
+
+            _context.Auctions.Remove(auction);
+
+            var result = await _context.SaveChangesAsync() > 0;
+            if (!result) return BadRequest("Failed to update auction");
+            return Ok();
         }
     }
 }
---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SearchService.Controllers
+{
+    [Route("api/search")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        
+    }
+}
