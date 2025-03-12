701ba1009de9262cf70a834bdbaeb7391aa1aa1e
Giovanny Choque Centeno <giovannyjchoquecenteno@gmail.com>
2025-03-12 11:08:35 -0400

    End of section 6

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -32,5 +32,18 @@ public static class Config
                 },
                 AllowedGrantTypes={GrantType.ResourceOwnerPassword}
             },
+            new Client{
+                ClientId = "nextApp",
+                ClientName= "nextApp",
+                ClientSecrets = {
+                    new Secret("secret".Sha256())
+                },
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                RequirePkce = false,
+                RedirectUris = {"https://localhost:3000/api/auth/callback/id-server"},
+                AllowOfflineAccess = true,
+                AllowedScopes = {"openid","profile","auctionApp"},
+                AccessTokenLifetime = 3600 * 24 *30
+            }
         };
 }
